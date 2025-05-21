1bb49784d7a2af83611fa25f609094cd35a1d671
Reduan413 <ridwanali3322@gmail.com>
2025-05-21 22:01:52 +0600

    update image

--- src/app/(pages)/(home)/components/HeroBanner.jsx
+++ src/app/(pages)/(home)/components/HeroBanner.jsx
@@ -5,7 +5,7 @@ const HeroBanner = () => {
     <div className=" static lg:fixed z-30 grid lg:h-screen w-full lg:w-[35.5%] lg:grid-rows-[90%_10%] bg-[#000000] lg:pl-[4.375rem] lg:pt-[76px] extraLg:pl-20 2xl:pl-[5.75rem] top-0 left-0">
       <div className="relative h-full w-full">
         <div className="absolute bottom-0 h-[4%] w-full bg-gradient-to-t from-[#d9d9d95d] to-transparent"></div>
-        <Image src="https://i.postimg.cc/LXzF5yDh/user-img.png" alt="" className="h-full w-full object-cover" width={500} height={500} />
+        <Image src="https://i.postimg.cc/W3WgFPn1/Mask-group-2.png" alt="" className="h-full w-full object-cover" width={500} height={500} />
       </div>
       <div className="h-full w-full py-5 lg:py-0">
         <Icons />
